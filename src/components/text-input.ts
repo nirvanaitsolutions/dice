87e5f7d95edfdb19a062e158faad22224e97c084
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-08 16:20:37 +1000

    chore(app): remove text input component

--- src/components/text-input.ts
+++
@@ -1,6 +0,0 @@
-import { customElement, bindable, bindingMode } from 'aurelia-framework';
-
-@customElement('text-input')
-export class TextInput {
-    @bindable({ defaultBindingMode: bindingMode.twoWay }) value = '';
-}
