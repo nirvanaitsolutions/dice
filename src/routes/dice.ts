6f7a90670e0dcf0cbd1153ce72dbe59db1731773
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-08 19:46:10 +1000

    feat(dice): beginning logic for dice and reactiveness

--- src/routes/dice.ts
+++ src/routes/dice.ts
@@ -1,7 +1,29 @@
-export class Dice {
-    minimumBet = 1;
+import { observable } from 'aurelia-binding';
 
-    sliderValue = 1;
-    sliderMinValue = 1;
-    sliderMaxValue = 100;
+type Direction = 'over' | 'under';
+
+export class Dice {
+    private minimumBet = 1;
+
+    @observable private sliderValue = 50;
+
+    private sliderMinValue = 1;
+    private sliderMaxValue = 100;
+
+    private winPercentage = 50;
+    private direction: Direction = 'under';
+
+    sliderValueChanged(value) {
+        if (this.direction === 'under') {
+            const percentage = (this.sliderMaxValue - value);
+            
+            if (value >= 50) {
+                this.winPercentage = percentage + 1;
+            } else {
+                this.winPercentage = percentage - 1;
+            }
+
+            console.log(percentage);
+        }
+    }
 }
