51e6d8740287cf411f3800e3a7cdac7f00329790
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-08 20:06:42 +1000

    fix(dice): proper percentage calculation

--- src/routes/dice.ts
+++ src/routes/dice.ts
@@ -7,23 +7,15 @@ export class Dice {
 
     @observable private sliderValue = 50;
 
-    private sliderMinValue = 1;
-    private sliderMaxValue = 100;
+    private sliderMinValue = 2;
+    private sliderMaxValue = 95;
 
     private winPercentage = 50;
     private direction: Direction = 'under';
 
     sliderValueChanged(value) {
-        if (this.direction === 'under') {
-            const percentage = (this.sliderMaxValue - value);
-            
-            if (value >= 50) {
-                this.winPercentage = percentage + 1;
-            } else {
-                this.winPercentage = percentage - 1;
-            }
-
-            console.log(percentage);
+        if (this.direction === 'under') {            
+            this.winPercentage = (value - 1);
         }
     }
 }
