d8d0f333494b6db79c84b206409e3a48b43b180f
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-09 12:36:28 +1000

    feat(dice): login method accepts a username

--- src/components/status.ts
+++ src/components/status.ts
@@ -1,4 +1,7 @@
 import { customElement } from 'aurelia-framework';
+import steem from 'steem';
+
+declare const steem_keychain: any;
 
 declare global {
     interface Window {
@@ -8,21 +11,18 @@ declare global {
 
 @customElement('status')
 export class Status {
-    login() {
+    login(username) {
         if (!window.steem_keychain) {
             window.alert('Steem Keychain extension not installed');
 
             return;
         }
 
-        const username = '';
-
         window.steem_keychain.requestSignBuffer(username, `dice_login-${Math.floor(100000000 + Math.random() * 900000000)}`, 'Posting', response => {
             if(response.success === true) {
-                const currentUser = ''; // get current username logged in
-                const username = response.data.username;
+                const returnedUser = response.data.username;
 
-                if (currentUser == username) {
+                if (returnedUser == username) {
                   // User already logged in
                   return;
                 }
