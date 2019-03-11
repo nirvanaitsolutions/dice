2a3a3a326ea85fa1155e0531b69559ddf067d0b0
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-11 10:09:15 +1000

    feat(components): app header component

---
+++ src/components/app-header.ts
@@ -0,0 +1,8 @@
+import { customElement } from 'aurelia-framework';
+
+import styles from './app-header.css';
+
+@customElement('app-header')
+export class AppHeader {
+    private styles = styles;
+}
