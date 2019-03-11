d00054dab689f78cd8e4d6245897a19bc4b23335
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-11 10:58:43 +1000

    refactor(components): remove app header

--- src/components/app-header.ts
+++
@@ -1,8 +0,0 @@
-import { customElement } from 'aurelia-framework';
-
-import styles from './app-header.css';
-
-@customElement('app-header')
-export class AppHeader {
-    private styles = styles;
-}
