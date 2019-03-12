6a3cd5868973f5998893093cd4a2fc054d3e92d3
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-12 14:42:29 +1000

    feat(resources): auth filter

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -4,6 +4,7 @@ import { FrameworkConfiguration } from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-      PLATFORM.moduleName('./attributes/class-attribute')
+      PLATFORM.moduleName('./attributes/class-attribute'),
+      PLATFORM.moduleName('./value-converters/auth-filter'),
   ]);
 }
