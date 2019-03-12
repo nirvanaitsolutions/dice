f1931faf068b78dec429ed362e5fcc5d057b4422
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-12 14:42:02 +1000

    feat(app): aurelia store plugin for state management

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 /// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
 
-import {Aurelia} from 'aurelia-framework'
+import { Aurelia } from 'aurelia-framework'
 import environment from './environment';
-import {PLATFORM} from 'aurelia-pal';
+import { PLATFORM } from 'aurelia-pal';
+
+import { initialState } from './store/state';
 
 import 'materialize-css';
 import '!style-loader!css-loader!materialize-css/dist/css/materialize.css';
@@ -10,21 +12,29 @@ import '!style-loader!css-loader!materialize-css/dist/css/materialize.css';
 import '!style-loader!css-loader!./styles/global.css';
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .feature(PLATFORM.moduleName('resources/index'));
+    aurelia.use
+        .standardConfiguration()
+        .feature(PLATFORM.moduleName('resources/index'));
 
-  aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
-  aurelia.use.plugin(PLATFORM.moduleName('aurelia-portal-attribute'));
-  aurelia.use.plugin(PLATFORM.moduleName('aurelia-materialize-bridge'), b => b.useAll());
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-store', 'store'), {
+        initialState: initialState,
+        history: {
+            undoable: false,
+            limit: 10
+        }
+    });
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-portal-attribute'));
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-materialize-bridge'), b => b.useAll());
 
-  if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
-  }
+    if (environment.debug) {
+        aurelia.use.developmentLogging();
+    }
 
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+    if (environment.testing) {
+        aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+    }
+
+    aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
 }
---
+++ src/store/actions.ts
@@ -0,0 +1,38 @@
+import { Container } from 'aurelia-framework';
+import { State } from './state';
+import store from './store';
+
+export async function login(state: State, user: any): Promise<State> {
+    let newState = { ...state };
+
+    newState.loggedIn = true;
+
+    return newState;
+}
+
+export async function logout(state: State): Promise<State> {
+    const newState = { ...state };
+
+    newState.loggedIn = false;
+
+    newState.user = {
+        id: null,
+        name: '',
+        balance: '',
+        sbd_balance: '',
+        can_vote: false,
+        post_count: 0,
+        voting_power: 0,
+        voting_manabar: {
+            current_mana: '0',
+            last_update_time: 0
+        },
+        reputation: 0,
+        valueInUsd: 0
+    };
+
+    return newState;
+}
+
+store.registerAction('login', login);
+store.registerAction('logout', logout);
