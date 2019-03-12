ffc8b940aa6ed924b43929fe50ff760e839f617d
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-12 14:57:29 +1000

    feat(auth): redirect user when logging out

--- src/components/status.ts
+++ src/components/status.ts
@@ -1,4 +1,4 @@
-import { customElement, computedFrom } from 'aurelia-framework';
+import { customElement, computedFrom, autoinject } from 'aurelia-framework';
 import { MdModal } from 'aurelia-materialize-bridge';
 
 import steem from 'steem';
@@ -6,13 +6,19 @@ import steem from 'steem';
 import styles from './status.css';
 import { dispatchify } from 'aurelia-store';
 import { login, logout } from 'store/actions';
+import { Router } from 'aurelia-router';
 
+@autoinject()
 @customElement('status')
 export class Status {
     private styles = styles;
     private loginUsername = localStorage.getItem('_dice_userNameCached') || null;
     private loggedInUser = this.getUser();
     private modal: MdModal;
+
+    constructor(private router: Router) {
+
+    }
 
     @computedFrom('loggedInUser.balance')
     get balance() {
@@ -94,5 +100,6 @@ export class Status {
         sessionStorage.removeItem('_dice_user');
         this.loggedInUser = null;
         dispatchify(logout)();
+        this.router.navigate('/');
     }
 }
