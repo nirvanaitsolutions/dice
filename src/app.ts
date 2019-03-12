c7a4f73b1a8ffce7de0caeec5d1f43eccab28b52
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-12 14:48:02 +1000

    fix(routes): titles were all wrong so labels were off

--- src/app.ts
+++ src/app.ts
@@ -30,8 +30,8 @@ export class App {
         config.map([
             { route: [''], name: 'home', title: 'Home', redirect: 'dice' },
             { route: 'dice', name: 'dice', title: 'Dice', moduleId: PLATFORM.moduleName('./routes/dice/dice'), nav: true },
-            { route: 'blackjack', name: 'blackjack', title: 'Dice', moduleId: PLATFORM.moduleName('./routes/blackjack/blackjack'), nav: true },
-            { route: 'slots', name: 'slots', title: 'Dice', moduleId: PLATFORM.moduleName('./routes/slots/slots'), nav: true },
+            { route: 'blackjack', name: 'blackjack', title: 'Blackjack', moduleId: PLATFORM.moduleName('./routes/blackjack/blackjack'), nav: true },
+            { route: 'slots', name: 'slots', title: 'Slots', moduleId: PLATFORM.moduleName('./routes/slots/slots'), nav: true },
 
             { route: 'my-bets', name: 'myBets', title: 'My Bets', moduleId: PLATFORM.moduleName('./routes/user/my-bets'), auth: true, nav: true }
         ]);
