4560ab58175769fa7c1fb525282b171e8efad876
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-13 13:53:56 +1000

    feat(app): async binding

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ export function configure(aurelia: Aurelia) {
     });
 
     aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-async-binding'));
     aurelia.use.plugin(PLATFORM.moduleName('aurelia-portal-attribute'));
     aurelia.use.plugin(PLATFORM.moduleName('aurelia-materialize-bridge'), b => b.useAll());
 
